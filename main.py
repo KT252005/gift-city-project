54f930cef911e7559eaa98d062f31ac849ea9af4
KT252005 <23f2005464@ds.study.iitm.ac.in>
2025-03-23 23:07:10 +0530

    main.py

--- main.py
+++ main.py
@@ -2,23 +2,40 @@ import ollama
 from database import Chroma
 from fastapi import FastAPI
 from pydantic import BaseModel
+import requests
 
 app=FastAPI()
+
 class QueryRequest(BaseModel):
     query: str
     store: str 
+    language : str
+
+def translate_mymemory(text, source_lang="en", target_lang="gu"):
+    url = "https://api.mymemory.translated.net/get"
+    params = {
+        "q": text,
+        "langpair": f"{source_lang}|{target_lang}"
+    }
+    headers = {"User-Agent": "Mozilla/5.0"}
+    response = requests.get(url, params=params,headers=headers)
+    
+    if response.status_code == 200:
+        data = response.json()
+        return data.get("responseData", {}).get("translatedText", "Translation failed")
+    else:
+        print(f"Error {response.status_code}: {response.text}")
+        return None
 
 def ollama_context(query,store):
     retrieved_data=Chroma.query_chroma(query,store=store)
     
     if not retrieved_data or "documents" not in retrieved_data or not retrieved_data["documents"]:
         return "No relevant context found in the database. Please provide more information."
-
-    
     
     context = "\n".join(retrieved_data["documents"][0])if retrieved_data["documents"] else ""
     
-    print(context)
+    
     prompt = f"""
     
     Context:\n{context}\n\n
@@ -33,9 +50,27 @@ def ollama_context(query,store):
     \nAnswer:"""
     
     response= ollama.chat(model="llama3.2:1b",messages=[{"role": "user", "content": prompt}])
+    print(response)
     return response["message"]["content"]
 
 @app.post("/query/")
 async def query_ollama(request:QueryRequest):
-    response_text =ollama_context(request.query,request.store)
+    print(request)
+    response_text =  ollama_context(request.query,request.store)
+    
+    print(response_text)
+    if request.language.lower() == "hindi":
+        translated_text = translate_mymemory(response_text,target_lang="hi")
+        print(translated_text)
+        return {"response": translated_text}
+
+    elif request.language.lower() == "gujarati":
+        translated_text =translate_mymemory(response_text,target_lang="gu")
+        print(translated_text)
+        return {"response": translated_text}
+
     return {"response": response_text}
+    
+ 
+    
+    
